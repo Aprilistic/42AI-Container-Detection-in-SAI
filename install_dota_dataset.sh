ec6006274e1bdd4bb103dfae04e2d58b59b7c691
Hochul Song <hocsong@cx2r8s4.42seoul.kr>
2023-07-16 16:50:21 +0900

    validation 이미지가 images/에 바로 옮겨지던 버그를 수정

--- install_dota_dataset.sh
+++ install_dota_dataset.sh
@@ -32,7 +32,7 @@ mv pytorch-rotation-decoupled-detector/images/train/images2/images/* pytorch-rot
 mv pytorch-rotation-decoupled-detector/images/train/images3/images/* pytorch-rotation-decoupled-detector/images/train/
 
 unzip pytorch-rotation-decoupled-detector/images/val/part1.zip -d pytorch-rotation-decoupled-detector/images/val/
-mv pytorch-rotation-decoupled-detector/images/val/images/* pytorch-rotation-decoupled-detector/images/
+mv pytorch-rotation-decoupled-detector/images/val/images/* pytorch-rotation-decoupled-detector/images/val
 
 unzip pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip -d pytorch-rotation-decoupled-detector/labelTxt/train
 unzip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip -d pytorch-rotation-decoupled-detector/labelTxt/val
@@ -40,3 +40,4 @@ unzip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip -d pytorch-
 rm -rf pytorch-rotation-decoupled-detector/images/train/images1 pytorch-rotation-decoupled-detector/images/train/images2 pytorch-rotation-decoupled-detector/images/train/images3
 rm -rf pytorch-rotation-decoupled-detector/images/val/images/
 rm -rf pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip
+rm -rf pytorch-rotation-decoupled-detector/labelTxt/train/*.zip
