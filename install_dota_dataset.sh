6b5a6a10c57611e0c012d2342fe31d8fbb2ba847
Hochul Song <hocsong@cx1r5s8.42seoul.kr>
2023-07-17 16:48:16 +0900

    불필요한 라인 주석처리

--- install_dota_dataset.sh
+++ install_dota_dataset.sh
@@ -21,25 +21,25 @@ mkdir -p pytorch-rotation-decoupled-detector/labelTxt/val
 
 gdown.pl/gdown.pl https://drive.google.com/file/d/1x5TQ02dlnEVbSkHLQ4yLmCoray8SAOYh/view?usp=drive_link container.zip
 
-mkdir pytorch-rotation-decoupled-detector/images/train/images1
-mkdir pytorch-rotation-decoupled-detector/images/train/images2
-mkdir pytorch-rotation-decoupled-detector/images/train/images3
+# mkdir pytorch-rotation-decoupled-detector/images/train/images1
+# mkdir pytorch-rotation-decoupled-detector/images/train/images2
+# mkdir pytorch-rotation-decoupled-detector/images/train/images3
 
-unzip pytorch-rotation-decoupled-detector/images/train/part1.zip -d pytorch-rotation-decoupled-detector/images/train/images1
-unzip pytorch-rotation-decoupled-detector/images/train/part2.zip -d pytorch-rotation-decoupled-detector/images/train/images2
-unzip pytorch-rotation-decoupled-detector/images/train/part3.zip -d pytorch-rotation-decoupled-detector/images/train/images3
+# unzip pytorch-rotation-decoupled-detector/images/train/part1.zip -d pytorch-rotation-decoupled-detector/images/train/images1
+# unzip pytorch-rotation-decoupled-detector/images/train/part2.zip -d pytorch-rotation-decoupled-detector/images/train/images2
+# unzip pytorch-rotation-decoupled-detector/images/train/part3.zip -d pytorch-rotation-decoupled-detector/images/train/images3
 
-mv pytorch-rotation-decoupled-detector/images/train/images1/images/* pytorch-rotation-decoupled-detector/images/train/ 
-mv pytorch-rotation-decoupled-detector/images/train/images2/images/* pytorch-rotation-decoupled-detector/images/train/
-mv pytorch-rotation-decoupled-detector/images/train/images3/images/* pytorch-rotation-decoupled-detector/images/train/
+# mv pytorch-rotation-decoupled-detector/images/train/images1/images/* pytorch-rotation-decoupled-detector/images/train/ 
+# mv pytorch-rotation-decoupled-detector/images/train/images2/images/* pytorch-rotation-decoupled-detector/images/train/
+# mv pytorch-rotation-decoupled-detector/images/train/images3/images/* pytorch-rotation-decoupled-detector/images/train/
 
-unzip pytorch-rotation-decoupled-detector/images/val/part1.zip -d pytorch-rotation-decoupled-detector/images/val/
-mv pytorch-rotation-decoupled-detector/images/val/images/* pytorch-rotation-decoupled-detector/images/val
+# unzip pytorch-rotation-decoupled-detector/images/val/part1.zip -d pytorch-rotation-decoupled-detector/images/val/
+# mv pytorch-rotation-decoupled-detector/images/val/images/* pytorch-rotation-decoupled-detector/images/val
 
-unzip pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip -d pytorch-rotation-decoupled-detector/labelTxt/train
-unzip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip -d pytorch-rotation-decoupled-detector/labelTxt/val
+# unzip pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip -d pytorch-rotation-decoupled-detector/labelTxt/train
+# unzip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip -d pytorch-rotation-decoupled-detector/labelTxt/val
 
-rm -rf pytorch-rotation-decoupled-detector/images/train/images1 pytorch-rotation-decoupled-detector/images/train/images2 pytorch-rotation-decoupled-detector/images/train/images3
-rm -rf pytorch-rotation-decoupled-detector/images/val/images/
-rm -rf pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip
-rm -rf pytorch-rotation-decoupled-detector/labelTxt/train/*.zip
+# rm -rf pytorch-rotation-decoupled-detector/images/train/images1 pytorch-rotation-decoupled-detector/images/train/images2 pytorch-rotation-decoupled-detector/images/train/images3
+# rm -rf pytorch-rotation-decoupled-detector/images/val/images/
+# rm -rf pytorch-rotation-decoupled-detector/labelTxt/train/labelv1_0.zip pytorch-rotation-decoupled-detector/labelTxt/val/labelv1_0.zip
+# rm -rf pytorch-rotation-decoupled-detector/labelTxt/train/*.zip
