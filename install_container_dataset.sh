146849464ed0f3d8a3b6dcbbd801e3b41474b8b3
Hochul Song <hocsong@cx1r5s8.42seoul.kr>
2023-07-17 19:17:54 +0900

    스크립트 개선

--- install_container_dataset.sh
+++ install_container_dataset.sh
@@ -4,11 +4,15 @@ gdown.pl/gdown.pl https://drive.google.com/file/d/1HtSqZqUSt5tbPtS6dLMfYBhijv8cJ
 
 unzip container.zip
 
-mv container/images pytorch-rotation-decoupled-detector/
+mv Archive/images pytorch-rotation-decoupled-detector/
 
-mv container/labelTxt pytorch-rotation-decoupled-detector/
+mv Archive/labelTxt pytorch-rotation-decoupled-detector/
 
-rm -rf container
+rm container.zip
+rm -rf Archive
 cd pytorch-rotation-decoupled-detector/images
-
 mv val/* test/
+
+cd ~
+mv 42AI/pytorch-rotation-decoupled-detector ./
+rm -rf 42AI
