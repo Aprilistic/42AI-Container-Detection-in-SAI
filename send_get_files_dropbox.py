d0a5d986b9916b1fc55d2907c33e693faa8bb7aa
Hochul Song <hocsong@cx2r1s1.42seoul.kr>
2023-07-18 18:54:11 +0900

    download/upload 모드 선택 가능하도록 개선

--- send_get_files_dropbox.py
+++ send_get_files_dropbox.py
@@ -18,8 +18,7 @@ def download_file(access_token, file_path, destination_path):
         print("File downloaded successfully.")
     else:
         print("Failed to download file.")
-
-
+        print(response.status_code)
 
 def upload_file(access_token, file_path, destination_path):
     url = "https://content.dropboxapi.com/2/files/upload"
@@ -37,11 +36,13 @@ def upload_file(access_token, file_path, destination_path):
         print("File uploaded successfully.")
     else:
         print("Failed to upload file.")
+        print(response.status_code)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Uploads/downloads a file to/from Dropbox.')
     # Add arguments
+    parser.add_argument('--download', type=bool, help='1 if you need to download. 0 otherwise.')
     parser.add_argument('--file_path', type=str, help='Path to the file to upload/download.')
     parser.add_argument('--destination_path', type=str, help='Where to download/upload. ex)/Dropbox/Folder/filt.txt')
 
@@ -50,6 +51,10 @@ if __name__ == '__main__':
 
     # Access the values of the arguments
     access_token = 'sl.BibA18uPA7FbmKS1eWz_NjjoBq1jBteOQYHUUKDDZgrzX2Nt0yZS7VD6Cl9ThfFBeUdSHVIKLhjqGwzeLFSIrubbV2iWj-MFknD3hcmLqmgL6_QuJNJW7EpTwmgyhQCiUJeGTy1h'
+    should_download = args.download
     file_path = args.file_path
     destination_path = args.destination_path
-    upload_file(access_token, file_path, destination_path)
+    if (should_download):
+        download_file(access_token, file_path, destination_path)
+    else:
+        upload_file(access_token, file_path, destination_path)
