8f3eebe5509c165ae32171864c55cc78ca2bc3f4
Hochul Song <hocsong@cx2r1s2.42seoul.kr>
2023-07-20 17:54:08 +0900

    DOTA dataset에서 학습할 수 있도록 config를  수정함

--- pytorch-rotation-decoupled-detector/config/__init__.py
+++ pytorch-rotation-decoupled-detector/config/__init__.py
@@ -2,7 +2,9 @@
 
 DIR_WEIGHT = '/root/pytorch-rotation-decoupled-detector/backbone_weights'
 
-CATEGORY_DOTA_V10 = ['container']
+CATEGORY_DOTA_V10 = ['baseball-diamond', 'basketball-court', 'bridge', 'ground-track-field', 'harbor', 'helicopter',
+                     'large-vehicle', 'plane', 'roundabout', 'ship', 'small-vehicle', 'soccer-ball-field',
+                     'storage-tank', 'swimming-pool', 'tennis-court']
 
 CATEGORY_UCAS_AOD = ['car', 'plane']
 
