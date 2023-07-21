95fd1870df56028e6ab40ca804db281c1efd0ab5
Hochul Song <hocsong@c4r4s8.42seoul.kr>
2023-07-21 10:31:07 +0900

    txt를 csv로 그리고 csv를 txt로 변환하는 스크립트 추가

---
+++ convert_txt_csv.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Uploads/downloads a file to/from Dropbox.')
+    # Add arguments
+    parser.add_argument('--target', type=str, help='The target file that you want to convert.')
+    parser.add_argument('--type', type=str, help="The type of the target. \'.txt\' if you want to convert txt to csv. \'.csv\' otherwise.")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Access the values of the arguments
+    target = args.target
+    type = args.type
+
+    with open(target) as infile:
+
+        # Read space-delimited file and replace all empty spaces by commas
+        if type == '.txt':
+            data = infile.read().replace(' ', ',')
+            print(data, file=open(target + '.csv', 'w'))
+        else:
+            data = infile.read().replace(',', ' ')
+            print(data, file=open(target + '.txt', 'w'))
+        # Write the CSV data in the output file
