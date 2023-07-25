1c0b3ceb019fac84b5dac327d9b6f1e9cf8be9c0
Hochul Song <hocsong@c4r5s6.42seoul.kr>
2023-07-25 10:49:29 +0900

    정리

--- scripts/find_intersection_area_test.py
+++
@@ -1,6 +0,0 @@
-from shapely.geometry import Polygon
-
-def intersection_area(rect1_coords, rect2_coords):
-    rect1_polygon, rect2_polygon = Polygon(rect1_coords), Polygon(rect2_coords)
-    intersection = rect1_polygon.intersection(rect2_polygon)
-    return intersection.area
